373b18ea92833820d8bc833c3cd5ee423cb1d0ec
R_SEIN <jaewon.rhea@gmail.com>
2021-02-15 07:25:28 +0000

    quiz 7

--- practice.py
+++ practice.py
@@ -1,10 +1,9 @@
-gun = 10
-
-def checkpoint(soldiers):
-    gun = 20
-    gun = gun - soldiers
-    print("[in function] Guns left: {0}".format(gun))
-
-print("Total Guns: {0}".format(gun))
-checkpoint(2)
-print("Guns left: {0}".format(gun))
+def std_weight(height, gender):
+    if gender == "male":
+        return height * height * 22
+    else:
+        return height * height * 21
+        
+height, gender = float(input()), input()
+weight = round(std_weight(height/100, gender), 2)
+print("Standard weight of {0} cm {1} is {2} kg.".format(height, gender, weight))
