877add9c5e7bc8302ba833510caefd72d145c50f
R_SEIN <jaewon.rhea@gmail.com>
2021-02-15 05:46:39 +0000

    global, local

--- practice.py
+++ practice.py
@@ -1,22 +1,13 @@
-from random import *
-aboard = 0
-for people in range(1, 51):
-    time = randint(5, 50)
-    if time in range(5, 16):
-        matching = "O"
-        aboard += 1
-    else:
-        matching = " "
-    print("[{0}] {1}번째 손님 (소요시간: {2}분)".format(matching, people, time))
-print("\n총 탑승 승객: {0}명".format(aboard))
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("Name: {0}\tAge: {1}\t".format(name, age), \
+#         lang1, lang2, lang3, lang4, lang5)
 
-from random import *
-aboard = 0
-for people in range(1, 51):
-    time = randrange(5, 51)
-    if 5 <= time <= 15:
-        print("[O] {0}번째 손님 (소요시간: {1}분)".format(people, time))
-        aboard += 1
-    else:
-        print("[ ] {0}번째 손님 (소요시간: {1}분)".format(people, time))
-print("\n총 탑승 승객: {0}명".format(aboard))
+def profile(name, age, *language):
+    print("Name: {0}\tAge: {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+
+profile("A", 20, "Python", "Java", "C", "C++", "C#", "Javas")
+profile("B", 25, "Kotlin", "Swift", "", "", "")
