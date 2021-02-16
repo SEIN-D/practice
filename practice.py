e601eb8e569de2ef5b46a5117d2393f92c8ed8c4
R_SEIN <jaewon.rhea@gmail.com>
2021-02-16 09:39:49 +0000

    02.16

--- practice.py
+++ practice.py
@@ -7,3 +7,19 @@ def std_weight(height, gender):
 height, gender = float(input()), input()
 weight = round(std_weight(height/100, gender), 2)
 print("Standard weight of {0} cm {1} is {2} kg.".format(height, gender, weight))
+
+import sys
+print("Python", "Java", file=sys.stdout)
+print("Python", "Java", file=sys.stderr)
+
+score = {"Math":0, "English":50, "Coding":100}
+for subject, score in score.items():
+    # print(subject, score)
+    print(subject.ljust(10), str(score).rjust(4), sep=":")
+
+for num in range(1, 21):
+    print("Waiting Number: " + str(num).zfill(3))
+
+answer = input("Type any input value: ")
+print(type(answer))
+print("Input value is {0}.".format(answer))
