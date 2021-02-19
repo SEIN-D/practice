b1635d1320a564ea182a622eb8c338be6c4d52ff
R_SEIN <jaewon.rhea@gmail.com>
2021-02-19 10:42:24 +0000

    Organizing Files

--- practice.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,51):
-    with open("Week {0}.txt".format(i), "w", encoding="utf8") as report:
-        report.write("- Weekly Report: Week {0} -".format(i))
-        report.write("Department: ")
-        report.write("Name: ")
-        report.write("Work Summary: ")
