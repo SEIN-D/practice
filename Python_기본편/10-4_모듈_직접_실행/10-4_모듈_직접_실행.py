f94a307813ffaf8fc679271c75010b355c107376
R_SEIN <jaewon.rhea@gmail.com>
2021-03-01 06:11:00 +0000

    모듈 직접 실행

---
+++ Python_기본편/10-4_모듈_직접_실행/10-4_모듈_직접_실행.py
@@ -0,0 +1,18 @@
+# import travel.thailand
+# # import travel.thailand.ThailandPackage
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# from random import *
+from travel import *
+# trip_to = vietnam.VietnamPackage()
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
