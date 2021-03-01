ce865177725e7197bbd7fd78ac4e8916ca1bd915
R_SEIN <jaewon.rhea@gmail.com>
2021-03-01 06:10:29 +0000

    패키지

---
+++ Python_기본편/10-2_패키지/10-2_패키지.py
@@ -0,0 +1,12 @@
+import travel.thailand
+# import travel.thailand.ThailandPackage
+trip_to = travel.thailand.ThailandPackage()
+trip_to.detail()
+
+from travel.thailand import ThailandPackage
+trip_to = ThailandPackage()
+trip_to.detail()
+
+from travel import vietnam
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
